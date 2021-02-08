34bd2cad86854e127bdc712f4b9f90c0662e937a
Mushfiq80 <aurpon80@gmail.com>
2021-02-08 19:50:46 +0600

    Next up

---
+++ Mid/evenadd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenadd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n = 100;
+            Console.WriteLine("The even numbers here(2-100): ");
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("{0} ", 2 * i);
+            }
+        }
+    }
+}
